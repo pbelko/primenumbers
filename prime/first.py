975afd04fafdb1c33261899dd437e73195c1124a
TonyT@Linux <tonymichael16@gmail.com>
2014-10-29 11:56:13 -0400

    Retry

---
+++ prime/first.py
@@ -0,0 +1,51 @@
+__author__ = 'tony'
+#print ("Hello World!")
+#print ("Hello Again")
+#print ("I like typing this.")
+#print ('Yay! Printing.')
+#print ("I'd much rather you 'not'.")
+#print ('I "said do not touch this.')
+# this is part of extra credit
+#print ('Print another line')
+# this is a comment
+# A comment
+# Anything after the # is ignored by Python
+print("I could have code like this.")
+
+# You can also use a comment to "disable" or out a piece of code
+# print ("This won't run")
+
+print("This will run.")
+
+print("I will now count my chickens:")
+
+# Precedence demonstration 30 / 6 = 5 + 25 = 30
+# and 25 * 3 = 75 % 4 = 3, and 100 - 3
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+print(3 + 2 < 5 - 7)
+
+print("Hens", 25.0 + 30.0 / 6.0)
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0)
+# This will print false
+print(3.0 + 2.0 < 5.0 - 7.0)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+
+# Will print the results of the calculation at the end of the String
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7", 5 - 7)
+
+print ("Oh, that's why it's False")
+
+print("How about some more.")
+
+# Will evaluate the truth of the expression
+print("Is it greater than?", 5 > -2)
+print("Is it greater than or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
+
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
---
+++ prime/prime.py
@@ -0,0 +1,34 @@
+__author__ = 'tony'
+import sys
+
+print(sys.argv)
+
+
+def main() -> object:
+    sentence = "Enter in a number to calculate: "
+    number = int(input(sentence))
+    calculate_prime(number)
+
+
+def calculate_prime(a):
+    a = 2
+    if a < 2:
+        sentence = 'The number { } must be greater than 2.'.format(a)
+        print(sentence)
+    for i in range(2, a):
+        if is_prime(i):
+            print(i)
+             # count += 1
+#        else:
+#            count += 1
+
+
+def is_prime(n):
+    for i in range(2, n):
+        if (n % i) == 0:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    main()
